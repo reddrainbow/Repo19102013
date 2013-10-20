3b7ac43a6572891e5b794a9d861d2983da9467ed
Vladimir Limar <livl@gmx.de>
2013-10-20 12:28:27 +0200

    Class for output

--- GitTestPr1/src/main/MainClass.java
+++ GitTestPr1/src/main/MainClass.java
@@ -1,12 +1,13 @@
 package main;
 
 import utils.Calculator;
+import utils.MyPrinter;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		int resultAdd = Calculator.addieren(2,3,4,5);
-
+		MyPrinter.myPrint("Result of Addition [2,3,4,5] = "+resultAdd);
 	}
 
 }
---
+++ GitTestPr1/src/utils/MyPrinter.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class MyPrinter {
+	public static void myPrint(String pMessage){
+		System.out.println(pMessage);
+	}
+}
