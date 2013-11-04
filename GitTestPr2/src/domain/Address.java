c6753c7345d707dec248a4a9c099e510f551fb93
Vladimir Limar <livl@gmx.de>
2013-11-04 01:01:55 +0100

    Validierung fuer Adresse

--- GitTestPr2/src/domain/Address.java
+++ GitTestPr2/src/domain/Address.java
@@ -1,17 +1,26 @@
 package domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Address {
 	private String strasse;
 	private String hausnummer;
 	private String plz;
 	private String ort;
+	private boolean valid;
 	public Address(String strasse, String hausnummer, String plz, String ort) {
 		super();
 		System.out.println(".... Creating Address Strasse:[ "+strasse+" ]; Hausnummer[ "+hausnummer+" ]; PLZ[ "+plz+" ]; Ort[ "+ort+" ]....");
-		this.strasse = strasse;
-		this.hausnummer = hausnummer;
-		this.plz = plz;
-		this.ort = ort;
+		if(validate(strasse, hausnummer, plz, ort)){
+			this.strasse = strasse;
+			this.hausnummer = hausnummer;
+			this.plz = plz;
+			this.ort = ort;
+			this.valid = true;
+		}else{
+			this.valid = false;
+		}
+		
 	}
 	public String getStrasse() {
 		return strasse;
@@ -38,6 +47,15 @@ public class Address {
 		this.ort = ort;
 	}
 	
+	public boolean validate(String strasse, String hausnummer, String plz, String ort){
+		boolean valid = StringUtils.isNotEmpty(strasse);
+		valid &= StringUtils.isNotEmpty(hausnummer);
+		valid &= StringUtils.isNotEmpty(plz);
+		valid &= StringUtils.isNotEmpty(ort);
+		
+		return valid ;
+	}
+	
 	@Override
 	public String toString(){
 		StringBuffer buf = new StringBuffer();
@@ -47,6 +65,12 @@ public class Address {
 		buf.append("\tPLZ: "+plz+"\n");
 		buf.append("\tOrt: "+ort);
 		return buf.toString();
+	}
+	public boolean isValid() {
+		return valid;
+	}
+	public void setValid(boolean valid) {
+		this.valid = valid;
 	}
 	
 }
