983d46036376940b1acf664d23ffc3e6868caaf6
Vladimir Limar HP <b2bconnections@gmx.de>
2014-01-15 08:50:48 +0100

    Commit HP 1

--- GitTestPr1/src/main/MainClass.java
+++ GitTestPr1/src/main/MainClass.java
@@ -8,6 +8,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		int resultAdd = Calculator.addieren(2,3,4,5);
 		MyPrinter.myPrint("Result of Addition [2,3,4,5] = "+resultAdd);
+		System.out.println("Hello from HP");
 	}
 
 }
